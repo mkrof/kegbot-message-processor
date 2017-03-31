56ed214be5a0b3844ebed3f449af45943000438d
mkrof <mkrof@users.noreply.github.com>
2017-03-31 13:15:37 -0400

    Verify function can receive token on request body.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 module.exports = function (context, req) {
 	context.log('JavaScript HTTP trigger function processed a request.');
 
-	if (req.query.name || (req.body && req.body.name)) {
+	if (req.body && req.body.token) {
 		context.res = {
 			// status: 200, /* Defaults to 200 */
-			body: "Hello " + (req.query.name || req.body.name)
+			body: "Token: " + (req.query.name || req.body.name)
 		};
-	}
-	else {
+	} else {
 		context.res = {
 			status: 400,
-			body: "Please pass a name on the query string or in the request body"
+			body: "Please pass a token on the request body"
 		};
 	}
 	context.done();
