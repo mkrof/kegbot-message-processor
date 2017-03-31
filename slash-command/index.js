ec408a132d45211309f7e0fd6685b6661fa4105d
mkrof <mkrof@users.noreply.github.com>
2017-03-31 15:22:31 -0400

    Attempt to get taps from server.

--- slash-command/index.js
+++ slash-command/index.js
@@ -3,23 +3,18 @@ const request = require('request-promise-native');
 
 const getAnswer = (question, userName) => new Promise((resolve, reject) => {
   if (question && question.toLowerCase().indexOf('tap') > -1) {
-    resolve('getting the beer list!');
-    // request('http://40.70.73.9:8000/api/taps')
-    //   .then(taps => taps.map(tap => {
-    //     resolve(taps);
-    //     const beverage = tap.current_keg.beverage;
-    //     resolve(
-    //       `A fine ${ beverage.style } produced by ${ beverage.producer.name }.`  
-    //     );
-    //   }))
-    //   .catch(() => resolve('Contact technical support!'));
+    request('http://kegberry-olson.eastus2.cloudapp.azure.com:8000/api/taps')
+      .then(taps => taps.map(tap => {
+        const beverage = tap.current_keg.beverage;
+        resolve(`A fine ${ beverage.style } produced by ${ beverage.producer.name }.`);
+      }))
+      .catch(() => resolve('Contact technical support!'));
   } else {
     resolve(`Good day you you, ${ userName }`);
   }
-})
+});
 
 module.exports = function (context, req) {
-
   const response = text => ({
     response_type: 'in_channel',
     text
@@ -30,7 +25,7 @@ module.exports = function (context, req) {
   if (body.token === 'uMdQWoCNsKmB3lPTbvHaEA31') {
     getAnswer(body.text, body.user_name)
       .then(answer => {
-        context.res = answer;
+        context.res = response(answer);
         context.done();
       })
       .catch(err => {
