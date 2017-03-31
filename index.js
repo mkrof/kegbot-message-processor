ef3038b7f018501386cf276fcbec57345ad3dd41
mkrof <mkrof@users.noreply.github.com>
2017-03-31 13:43:31 -0400

    Format response correctly.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 module.exports = function (context, req) {
 	context.log('JavaScript HTTP trigger function processed a request.');
 
+  const response = text => ({
+    response_type: 'in_channel',
+    text
+  });
+
 	if (req.body && req.body.token) {
-		context.res = {
-			// status: 200, /* Defaults to 200 */
-			body: "Token: " + req.body.token
-		};
+    context.res = response(`Token: ${ req.body.token }`);
+		// context.res = {
+		// 	// status: 200, /* Defaults to 200 */
+		// 	body: "Token: " + req.body.token
+		// };
 	} else {
-		context.res = {
-			status: 400,
-			body: "Please pass a token on the request body"
-		};
+    context.res = response('Token required.');
+		// context.res = {
+		// 	status: 400,
+		// 	body: "Please pass a token on the request body"
+		// };
 	}
 	context.done();
 };
