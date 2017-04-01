aa5fd1df243c847bdb83ec1b7392eefe3fb1176b
mkrof <mkrof@users.noreply.github.com>
2017-04-01 19:30:52 -0400

    Add rudimentary message parsing.

--- slash-command/index.js
+++ slash-command/index.js
@@ -2,21 +2,64 @@ const qs = require('qs');
 const request = require('request-promise-native');
 
 const getAnswer = (question, userName) => new Promise((resolve, reject) => {
-  if (question && question.toLowerCase().indexOf('tap') > -1) {
-    request('http://kegberry-olson.eastus2.cloudapp.azure.com:8000/api/taps')
-      .then(taps => taps.map(tap => {
+  if (!question || typeof question !== 'string') {
+    resolve(`Good day to you, ${ userName }. :beer:`);
+  } else if (question.toLowerCase().indexOf('cheers') > -1) {
+    resolve(`:beers:`);
+  } else if (question.toLowerCase().indexOf('help') > -1) {
+    resolve(`
+      Good day to you, ${ userName }. :beer:\n
+      Try asking me something like:\n
+      - What's on tap?\n
+      - How much is left?
+    `);
+  } else if (question.toLowerCase().indexOf('dance') > -1) {
+    resolve(`:dancers:`);
+  } else if (question.toLowerCase().indexOf('tap') > -1) {
+    request({
+      uri:'http://kegberry-olson.eastus2.cloudapp.azure.com:8000/api/taps',
+      qs: {
+        api_key: '3a2e3bb8409d4d1a9913e7f9bd166583'
+      },
+      json: true
+    }).then(taps => taps.objects.map(tap => {
         const beverage = tap.current_keg.beverage;
         resolve(`A fine ${ beverage.style } produced by ${ beverage.producer.name }.`);
       }))
       .catch(() => resolve('Contact technical support!'));
-  } else {
-    resolve(`Good day you you, ${ userName }`);
+  } else if (
+    question.toLowerCase().indexOf('status') > -1
+    || (question.toLowerCase().indexOf('beer') > -1 && question.toLowerCase().indexOf('left') > -1)
+    || (question.toLowerCase().indexOf('much') > -1 && question.toLowerCase().indexOf('left') > -1)
+  ) {
+    request({
+      uri:'http://kegberry-olson.eastus2.cloudapp.azure.com:8000/api/kegs',
+      qs: {
+        api_key: '3a2e3bb8409d4d1a9913e7f9bd166583'
+      },
+      json: true
+    }).then(kegs => kegs.objects.map(keg => {
+        const remaining = keg.percent_full;
+        const emoji = (percentFull => {
+          let e = ':scream:';
+          if (percentFull > 10) e = ':dizzy_face:';
+          if (percentFull > 20) e = ':cold_sweat:';
+          if (percentFull > 40) e = ':worried:';
+          if (percentFull > 60) e = ':slightly_smiling_face:';
+          if (percentFull > 85) e = ':smile:';
+          return e;
+        })(remaining);
+        resolve(`*${ remaining.toPrecision(3) } percent remaining! ${ emoji }`);
+      }))
+      .catch(() => resolve('Contact technical support!'));
+
   }
 });
 
 module.exports = function (context, req) {
   const response = text => ({
     response_type: 'in_channel',
+    mrkdwn: true,
     text
   });
 
