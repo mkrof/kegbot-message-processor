9fbf0242fa8a5da97c899291665ecf8fc3080ef8
mkrof <mkrof@users.noreply.github.com>
2017-04-04 08:42:50 -0400

    Add function.json.

--- caffeine/index.js
+++ caffeine/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const slashCommand = 'https://olson-kegbot-slack-processor.azurewebsites.net/api/slash-command?code=GnMVP80D9GFI6IBnjDqmYVgSJORPwBeA2/RKlPaEhBqRf7XLL9yopg==';
 
-module.exports = function (context, req) {
+module.exports = function (context, timer) {
   http.get(slashCommand, res => {
     context.log(`SUCCESS ${ res }` );
     context.done();
