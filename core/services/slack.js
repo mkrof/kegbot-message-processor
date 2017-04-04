4d15d1489b36084cf44c9da06f892553760143d0
mkrof <mkrof@users.noreply.github.com>
2017-04-04 15:24:36 -0400

    Move slash command func url to environment variable.

--- core/services/slack.js
+++ core/services/slack.js
@@ -1,6 +1,6 @@
 const request = require('request-promise-native');
 
-const slashCommandUrl = 'https://olson-kegbot-slack-processor.azurewebsites.net/api/slash-command?code=GnMVP80D9GFI6IBnjDqmYVgSJORPwBeA2/RKlPaEhBqRf7XLL9yopg==';
+const slashCommandUrl = process.env.SLASH_COMMAND_FUNCTION_URL;
 
 const message = text => ({
   response_type: 'in_channel',
