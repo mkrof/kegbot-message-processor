f3de08c5fd413bd21dd86cab6bab8a87298e40d4
mkrof <mkrof@users.noreply.github.com>
2017-04-04 12:55:28 -0400

    Add caffeine function.

--- caffeine/index.js
+++ caffeine/index.js
@@ -1,10 +1,16 @@
-const https = require('https');
+const request = require('request-promise-native');
 
 const slashCommand = 'https://olson-kegbot-slack-processor.azurewebsites.net/api/slash-command?code=GnMVP80D9GFI6IBnjDqmYVgSJORPwBeA2/RKlPaEhBqRf7XLL9yopg==';
 
 module.exports = function (context, timer) {
-  https.get(slashCommand, res => {
-    context.log(`SUCCESS ${ res }` );
-    context.done();
-  }).on('error', err => context.log(`ERROR: ${ err }`));
+  Promise.all([
+    request.get(slashCommand)
+  ]).then(() => {
+      context.log('success');
+      context.done()
+    })
+    .catch(err => {
+      context.log(err.message);
+      context.done();
+    });
 }
