770c867056543b53fc99b64907dc15f2402203f9
mkrof <mkrof@users.noreply.github.com>
2017-04-04 08:51:15 -0400

    Use https.

--- caffeine/index.js
+++ caffeine/index.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const https = require('https');
 
 const slashCommand = 'https://olson-kegbot-slack-processor.azurewebsites.net/api/slash-command?code=GnMVP80D9GFI6IBnjDqmYVgSJORPwBeA2/RKlPaEhBqRf7XLL9yopg==';
 
 module.exports = function (context, timer) {
-  http.get(slashCommand, res => {
+  https.get(slashCommand, res => {
     context.log(`SUCCESS ${ res }` );
     context.done();
   }).on('error', err => context.log(`ERROR: ${ err }`));
