97117bf40c4c72629da85bbf80b2ff0ac3d3846f
mkrof <mkrof@users.noreply.github.com>
2017-04-28 13:18:51 -0400

    Print body.

--- slash-command/index.js
+++ slash-command/index.js
@@ -44,10 +44,10 @@ const getAnswer = (body) => new Promise((resolve, reject) => {
 
 module.exports = function (context, req) {
   const body = qs.parse(req.body);
-  context.res = {
-    text: 'hello'
-  };
+  let res = Object.keys(body).map(key => `${ key }: ${ body[key] }\n`).join('');
+  context.res = res;
   context.done();
+
   if (process.env.SLACK_CHANNEL_NAMES.split(' ').indexOf(body.channel_name) === -1) {
     context.log(`Kegbot not available in #${ body.channel_name }`);
     context.done();
